d0e55ac6a932409d8ecfc5e079b37c1d38be2dfd
yonatan_betito <yonatanbetito@gmail.com>
2025-06-16 10:18:58 +0300

    16_06

--- FinalProject/Abstract/Sensors.cs
+++ FinalProject/Abstract/Sensors.cs
@@ -1,13 +1,13 @@
 namespace FinalProject.Interface;
 
-public abstract class AbsSensoris
+public abstract class Sensors
 {
 
     public string NameOfSensor { get; protected set; }
     public bool IsActive { get; protected set; }
     public int NumberOfActivetion { get;protected set; }
     
-    protected AbsSensoris(string nameSensor)
+    protected Sensors(string nameSensor)
     {
         NameOfSensor = nameSensor;
         NumberOfActivetion = 0;
